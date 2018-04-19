793bad602e5d59bd327c7acd12002802d077599f
Russell Gordon <rgordon@lcs-mba2410.lcs.on.ca>
2018-04-19 08:55:48 -0400

    Initial Commit

---
+++ IntroToDictionaries/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  IntroToDictionaries
+//
+//  Created by Gordon, Russell on 2018-04-19.
+//  Copyright © 2018 Gordon, Russell. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
